8f0eb477f2ac54ab6c44b5d7d2d8a6f04251e9bd
Pi Rson <duwunaung@gmail.com>
2024-04-10 13:50:58 +0630

    Add Model
    # Model for Author
    # Model for Blog
    # Model for Tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Author, Tag, Blog
 # Register your models here.
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "name", "email")
+
+class BlogAdmin (admin.ModelAdmin):
+    list_filter = ("author", "tags",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag)
+admin.site.register(Blog, BlogAdmin)
